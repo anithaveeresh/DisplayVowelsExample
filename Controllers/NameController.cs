d2d26ca3d383ff911c23b460673cd3b8107d49ee
anithaveeresh <ani.kalpa@gmail.com>
2022-02-07 17:56:59 +0530

    Initial GitHub Upload

---
+++ Controllers/NameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Display_Vowels.Controllers
+{
+    public class NameController : Controller
+    {
+        // GET: Name
+        public ActionResult Index()
+        {
+
+            var tempName = new char[]{'a', 'n', 'i', 't','h','a'};
+            var englishVowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            string vowelsInName = string.Empty;
+
+            foreach (var letters in tempName)
+            {
+                if (englishVowels.Any(x => x == letters))
+                {
+                    vowelsInName = string.Format("{0}_{1}", vowelsInName, letters);
+                }
+            }
+
+            ViewBag.VowelsInName = vowelsInName;
+            return View();
+        }
+    }
+}
